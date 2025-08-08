85b8bff1c9f6b6f7b460b72d25f7cedd694597e7
DBinK <DBinKv1@Gmail.com>
2025-08-08 17:22:22 +0800

    docs(README): 更新使用示例并调整代码格式
    - 在 README.md 中添加常规使用方式和上下文使用方式的示例代码
    - 更新 manual_usage.py 和 with_usage.py 示例文件，简化代码结构
    - 调整代码格式以提高可读性

--- examples/manual_usage.py
+++ examples/manual_usage.py
@@ -1,14 +1,13 @@
 from looptick import LoopTick
 import time
 
-def main():
-    timer = LoopTick(auto_report=False)
-    for i in range(5):
-        diff = timer.tick()
-        print(f"第 {i} 次循环耗时: {diff * timer.NS2MS:.6f} ms")
-        time.sleep(0.01)
-    print(f"总耗时: {timer.total_sec:.6f} 秒")
-    print(f"平均耗时: {timer.average_ms:.6f} ms")
+looptick = LoopTick()
 
-if __name__ == "__main__":
-    main()
+for i in range(5):
+    diff = looptick.tick()
+    print(f"第 {i} 次循环耗时: {diff * looptick.NS2MS:.6f} ms")
+    time.sleep(0.01)
+
+    
+print(f"总耗时: {looptick.total_sec:.6f} 秒")
+print(f"平均耗时: {looptick.average_ms:.6f} ms")
--- examples/with_usage.py
+++ examples/with_usage.py
@@ -1,12 +1,8 @@
 from looptick import LoopTick
 import time
 
-def main():
-    with LoopTick() as timer:
-        for i in range(5):
-            diff = timer.tick()
-            print(f"第 {i} 次循环耗时: {diff * timer.NS2MS:.6f} ms")
-            time.sleep(0.01)
-
-if __name__ == "__main__":
-    main()
+with LoopTick() as looptick:
+    for i in range(5):
+        diff = looptick.tick()
+        print(f"第 {i} 次循环耗时: {diff * looptick.NS2MS:.6f} ms")
+        time.sleep(0.01)
